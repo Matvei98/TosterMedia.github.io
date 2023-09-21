7efdcb90df151653cd7b8cd46f0c5b6f82fa8799
Matvei98 <Matthias78@yandex.ru>
2023-09-21 20:00:45 +0300

    k

--- js/script.js
+++ js/script.js
@@ -5,13 +5,10 @@ const galleryImages = document.querySelectorAll('.gallery-image');
 const previewImage = document.querySelector('.preview-image');
 const previewContainer = document.querySelector('.preview');
 
-// Добавляем обработчик события клика для каждой картинки
 galleryImages.forEach((image) => {
     image.addEventListener('click', () => {
-        // Получаем значение атрибута data-src для текущей картинки
         const dataSrc = image.getAttribute('data-src');
         if (dataSrc) {
-            // Устанавливаем src превью равным значению data-src
             previewImage.src = dataSrc;
             previewContainer.style.display = 'block';
         }
@@ -23,10 +20,7 @@ previewImage.addEventListener('click', () => {
 
 
 const buyButton = document.getElementById("buyButton");
-
-// Устанавливаем обработчик события на клик по кнопке
 buyButton.addEventListener("click", function() {
-  // Переходим по ссылке на youtube.com
   window.location.href = "https://www.youtube.com";
 });
 
--- js/scriptTimer.js
+++ js/scriptTimer.js
@@ -1,11 +1,9 @@
 const targetTime = new Date("2023-12-31T23:59:59").getTime();
 
-// Получаем элементы таймера
 const hoursElement = document.querySelector(".timer__hours");
 const minutesElement = document.querySelector(".timer__minutes");
 const secondsElement = document.querySelector(".timer__seconds");
 
-// Обновление таймера каждую секунду
 const timerInterval = setInterval(updateTimer, 1000);
 
 function updateTimer() {
@@ -20,17 +18,14 @@ function updateTimer() {
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    // Обновляем значения элементов таймера
     hoursElement.textContent = padZero(hours);
     minutesElement.textContent = padZero(minutes);
     secondsElement.textContent = padZero(seconds);
   }
 }
 
-// Функция для добавления нуля перед числами < 10
 function padZero(number) {
   return (number < 10) ? `0${number}` : number;
 }
 
-// Инициализация таймера при загрузке страницы
 updateTimer();
